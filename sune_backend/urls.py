03d2ef1f9edbd6acc9af18df685f64a932959b98
Harrison-Muraya <harrisonmuraya8@gmail.com>
2026-02-10 14:05:23 +0300

    Added Swagger/OpenAPI documentation and media serving in development

--- sune_backend/urls.py
+++ sune_backend/urls.py
@@ -15,8 +15,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Swagger/OpenAPI documentation
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Sune TV API",
+        default_version='v1',
+        description="REST API for Sune TV streaming application",
+        terms_of_service="https://www.sunetv.com/terms/",
+        contact=openapi.Contact(email="api@sunetv.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Admin
+    path('admin/', admin.site.URLs),
+    
+    # API endpoints
+    path('api/', include('streams.urls')),
+    
+    # API Documentation
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
